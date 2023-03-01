a92dc7b04bc41eca894a37a192bcb03aa2037674
Bruno Borges <bborgesfilho33@gmail.com>
2023-03-01 16:56:19 -0300

    Adjusting firebase config

--- src/firebaseConfig.jsx
+++ src/firebaseConfig.jsx
@@ -3,12 +3,7 @@ import { getAuth } from 'firebase/auth';
 import { getFirestore } from 'firebase/firestore';
 
 export const firebaseConfig = {
-	apiKey: 'AIzaSyD66zmgx_VRYPfjwsFxeBMNZ-JkKQlpJlU',
-	authDomain: 'whatsappclone-84114.firebaseapp.com',
-	projectId: 'whatsappclone-84114',
-	storageBucket: 'whatsappclone-84114.appspot.com',
-	messagingSenderId: '973728135944',
-	appId: '1:973728135944:web:52c9e8f2916a43b9f3cabd',
+	/* Insert your firebase configurations here */
 };
 
 const app = initializeApp(firebaseConfig);
